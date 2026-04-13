6bc5fff346d21370a901924de963398472bee3ec
zoefisk <zoejewelfisk@gmail.com>
2026-04-13 11:04:44 -0400

    chore: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -6,6 +9,11 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  // {
+  //   rules: {
+  //     "no-console": "warn",
+  //   },
+  // }
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
@@ -13,11 +21,7 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
-  // {
-  //   rules: {
-  //     "no-console": "warn",
-  //   },
-  // }
+  ...storybook.configs["flat/recommended"]
 ]);
 
 export default eslintConfig;
